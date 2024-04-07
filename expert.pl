6830f99c798eab4e0b7a8e1c2ba3222bdac9cb99
Liet3 <joshua.mark@uni.minerva.edu>
2024-04-07 15:15:17 -0700

    add some cafes, modify recommendation requirements

--- expert.pl
+++ expert.pl
@@ -8,39 +8,55 @@ recommend(acr):- \+wifi(strong),
 	travel_distance(X), X >= 0.5,
 	\+arrive_time(evening),
 	\+arrive_time(late_evening),
-	\+dinner,
-	length_stay(X), X =< 2.
+	\+dinner.
 
 %Maria's market cafe
-recommend(mmc):- noise_level(moderate), arrive_time(morning).
-recommend(mmc):- noise_level(moderate), arrive_time(noon).
+recommend(mmc):- noise_level(moderate),
+	diet(none),
+	\+arrive_time(evening),
+	travel_distance(X), X >= 2.8,
+	\+breakfast,
+	\+dinner.
 
 %The English Rose Café and Tea Shop
 recommend(erc):- \+computer(need_charge), \+budget_category(one),
 	noise_level(moderate),
-	travel_distance(X), X >= 3.7,
 	\+arrive_time(evening),
 	\+arrive_time(late_evening),
-	\+dinner,
-	length_stay(X), X =< 1.
+	travel_distance(X), X >= 3.7,
+	\+lunch,
+	\+dinner.
 
 recommend(mola_cafe):- \+budget_category(one),
 	noise_level(quiet),
+	\+arrive_time(evening),
+	\+arrive_time(late_evening),
+	travel_distance(X), X >= 0.5,
+	\+dinner.
+
+%Gecko Coffeehouse
+recommend(gecko):- \+budget_category(one),
+	noise_level(quiet),
+	\+arrive_time(evening),
+	\+arrive_time(late_evening),
+	travel_distance(X), X >=1.3,
+	\+dinner.
+
+%a pinch of salt
+recommend(pinch):- \+budget_category(one),
+	noise_level(noisy),
 	travel_distance(X), X >= 0.5,
 	\+arrive_time(evening),
 	\+arrive_time(late_evening),
-	\+dinner,
-	length_stay(X), X =< 2.
-
+	\+dinner.
 
 wifi(strong):- work(meeting).
-wifi(strong):- ask(wifi, X), X == yes. % yes or no
+wifi(X):- ask(wifi, X). % strong, normal, no_wifi
 noise_level(quiet):- wifi(strong).
 noise_level(X):- ask(noise_level, X). %noise_level can have values quiet, moderate, noisy (or maybe dont_care for last option)
 work(X):- ask(work, X). % work can have values meeting, no_meeting, none
-computer(X):- ask(computer, X). % computer can have values need_charge
-diet(X):- ask(diet, X). % diet can have values vegan, vegt, gluten_free. Should we
-%recommend a vegan place to only vegans or anyone generally?
+computer(X):- ask(computer, X). % computer can have values need_charge, no_need_charge
+diet(X):- ask(diet, X). % diet can have values vegan, vegt, gluten_free, none.
 arrive_time(X):- ask(arrive_time, X). % arrive_time can be very_early, morning, noon, evening, late_evening, night
 breakfast:- ask(breakfast, X), X \== no.
 lunch:- ask(lunch, X), X \== no.
