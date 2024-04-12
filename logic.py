64b97152da575acb342bd77ececb2d8c7e72c69c
Liet3 <joshua.mark@uni.minerva.edu>
2024-04-12 08:37:40 -0700

    rendered recommendation on tui

--- logic.py
+++ logic.py
@@ -3,128 +3,6 @@ import json
 from pyswip.prolog import Prolog
 from pyswip.easy import Functor, Variable, registerForeign, call, Atom
 
-askables = {
-    "init_tui": {
-        "text": "Init",
-        "options": [],
-        "default": "Init",
-        "type": "single_choice"
-    },
-    "work": {
-        "text": "Do you wish to work at the cafe?",
-        "options": [
-            "meeting",
-            "no_meeting",
-            "none"
-        ],
-        "default": "none",
-        "type": "single_choice"
-    },
-    "wifi": {
-        "text": "What strength of wifi do you need (if any)?",
-        "options": [
-            "strong",
-            "normal",
-            "none"
-        ],
-        "default": "none",
-        "type": "single_choice"
-    },
-    "noise_level": {
-        "text": "What is the maximum noise level you can tolerate?",
-        "options": [
-            "quiet",
-            "moderate",
-            "noisy"
-        ],
-        "default": "quiet",
-        "type": "single_choice"
-    },
-    "computer": {
-        "text": "Are you the responsible type or type to forget to charge your computer before going out?",
-        "options": [
-            "need_charge",
-            "no_need_charge"
-        ],
-        "default": "need_charge",
-        "type": "single_choice"
-    },
-    "diet_restrictions": {
-        "text": "What dietary restrictions do you have?",
-        "options": [
-            "vegan",
-            "vegt",
-            "gluten_free",
-            "none"
-        ],
-        "default": "none",
-        "type": "multi_choice"
-    },
-    "arrive_times": {
-        "text": "At what times do you want to go to the cafe?",
-        "options": [
-            "morning",
-            "afternoon",
-            "evening"
-        ],
-        "default": "afternoon",
-        "type": "multi_choice"
-    },
-    "breakfast": {
-        "text": "Do you want to have breakfast at the cafe?",
-        "options": [
-            "yes",
-            "no"
-        ],
-        "default": "no",
-        "type": "single_choice"
-    },
-    "lunch": {
-        "text": "Do you want to have lunch at the cafe?",
-        "options": [
-            "yes",
-            "no"
-        ],
-        "default": "no",
-        "type": "single_choice"
-    },
-    "dinner": {
-        "text": "Do you want to have dinner at the cafe?",
-        "options": [
-            "yes",
-            "no"
-        ],
-        "default": "no",
-        "type": "single_choice"
-    },
-    "length_stay": {
-        "text": "How long do you plan to stay in a cafe?",
-        "options": [
-            "1.0"
-        ],
-        "default": "1.0",
-        "type": "single_choice"
-    },
-    "travel_distance": {
-        "text": "How far are you willing to travel from the residence hall (maximum)?",
-        "options": [
-            "10.0"
-        ],
-        "default": "10.0",
-        "type": "single_choice"
-    },
-    "budget_category": {
-        "text": "What is your budget for a visit to the cafe?",
-        "options": [
-            "one",
-            "two",
-            "three"
-        ],
-        "default": "two",
-        "type": "single_choice"
-    }
-}
-
 def read_input(A, V, Y):
     """A is the askable, V is the value prolog wants to find out
     about (unused here but needed), Y is the return variable to prolog"""
@@ -141,14 +19,14 @@ def read_input(A, V, Y):
         return False
 read_input.arity = 3
 
-
-
 def run_expert_system(shared_data):
     prolog = Prolog() # Global handle to interpreter
     retractall = Functor("retractall")
     known = Functor("known",2)
-    # with open("askables.json") as f:
-    #     askables = json.load(f)
+    with open("askables.json") as f:
+        askables = json.load(f)
+    with open("cafes.json") as f2:
+        cafes = json.load(f2)
 
     def read_input2(A, V, Y):
         if isinstance(Y, Variable):
@@ -168,7 +46,8 @@ def run_expert_system(shared_data):
             if askable in ["diet_restrictions", "arrive_times"]:
                 Y.unify(list(map(Atom, response)))
             elif askable in ["travel_distance", "length_stay"]:
-                Y.unify(float(response[0]))
+                value_opt = next(opt for opt in ask_dict["options"] if opt["id"] == response[0])
+                Y.unify(float(value_opt["value"]))
             else:
                 Y.unify(Atom(response[0]))
             return True
@@ -181,8 +60,12 @@ def run_expert_system(shared_data):
     prolog.consult("expert.pl") # open the KB for consulting
     call(retractall(known))
     rec = [s for s in prolog.query("recommend(X)", maxresult=1)]
+    shared_data["done"] = True
+    rec_id = rec[0]['X'] if rec else None
+    shared_data["rec"] = cafes[rec_id] if rec_id else None
+    shared_data["rec"]["id"] = rec_id
     with open("rec.txt", 'w') as f:
-        f.write(f"I recommend {rec[0]['X']}" if rec else "No recommendation.")
+        f.write(f"I recommend {rec_id}" if rec else "No recommendation.")
         f.write(str(list(prolog.query("known(A, V)"))))
 
 #   
--- main.py
+++ main.py
@@ -41,6 +41,7 @@ shared_data = {"curr_question": {"text": "Hello", "type": "single_choice", "opti
 class RadioSetChangedApp(App[None]):
     CSS_PATH = "tut.tcss"
     selected_value = reactive(shared_data["curr_question"]["default"])
+    about_to_finish = False
 
     def compose(self) -> ComposeResult:
         # with VerticalScroll():
@@ -48,46 +49,56 @@ class RadioSetChangedApp(App[None]):
             yield Label(shared_data["curr_question"]["text"], id="curr_question")
         with Horizontal():
             with RadioSet(id="focus_me"):
-            	for opt in shared_data["curr_question"]["options"]:
-            		yield RadioButton(opt,)
+                for opt in shared_data["curr_question"]["options"]:
+                    yield RadioButton(opt["text"], id=opt["id"])
                 
         with Horizontal():
-            yield Label(self.selected_value, id="selected")
-            yield Button("Next", id="start", variant="success")
+            #yield Label(self.selected_value, id="selected")
+            yield Button("Next", id="next_btn", variant="success")
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
+        if self.about_to_finish:
+            self.exit()
+            return
         shared_data[f"{shared_data['curr_question']['id']}_response"] = [
             self.selected_value
         ]
         time.sleep(0.1)  # maybe wait for prolog?
-        self.query_one("#curr_question", Label).update(shared_data["curr_question"]["text"])
+        if not shared_data.get("done"):
+            self.query_one("#curr_question", Label).update(shared_data["curr_question"]["text"])
         for btn in self.query("RadioButton"):
-        	btn.remove()
-        for opt in shared_data["curr_question"]["options"]:
-        	radioset = self.query_one("#focus_me")
-        	radioset.mount(RadioButton(opt, )) # value = opt==shared_data["curr_question"]["default"]
-        	radioset.focus()
+            btn.remove()
+        if shared_data.get("done"):
+            link = shared_data["rec"]["link"]
+            self.query_one("#focus_me").remove()
+            self.query_one("#next_btn", Button).label = "Finish"
+            self.query_one("#curr_question", Label).update(
+                f"I recommend {shared_data['rec']['name']}." + \
+                ("" if not link else f" Check out the cafe here: {link}")
+            )
+            self.about_to_finish = True
+        else:
+            for opt in shared_data["curr_question"]["options"]:
+                radioset = self.query_one("#focus_me")
+                radioset.mount(RadioButton(opt["text"], id=opt["id"])) # value = opt==shared_data["curr_question"]["default"]
+                radioset.focus()
 
     def on_mount(self) -> None:
         self.query_one(RadioSet).focus()
 
     def on_radio_set_changed(self, event: RadioSet.Changed) -> None:
-        self.selected_value = str(event.pressed.label)
-        self.query_one("#selected", Label).update(
-            f"Selected label: {self.selected_value}"
-        )
+        self.selected_value = str(event.pressed.id)
+        # self.query_one("#selected", Label).update(
+        #     f"Selected label: {self.selected_value}"
+        # )
 
 
 if __name__ == "__main__":
     manager = Manager()
     shared_data = manager.dict({"curr_question": {"text": "Hello", "type": "single_choice", "options": [], "default": "None"}})
     p1 = Process(target=run_expert_system, args=(shared_data,))
-    # p2 = Process(target=app_func, args=(shared_data,))
     p1.start()
-
     RadioSetChangedApp().run()
-    # p2.start()
     p1.join()
-    # p2.join()
 
 
