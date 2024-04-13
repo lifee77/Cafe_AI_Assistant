43a54a5a2814f78705360ef7f52fa5368ba85ec2
Dilnaz Uasheva <50591474+dilnazua@users.noreply.github.com>
2024-04-13 15:54:26 +0200

    Added some comments
    @Liet3 @L-iet can you review them? I will probably add more

--- logic.py
+++ logic.py
@@ -7,16 +7,23 @@ from pyswip.easy import Functor, Variable, registerForeign, call, Atom
 def run_expert_system(shared_data):
     prolog = Prolog()  # Global handle to interpreter
     retractall = Functor("retractall")
+    # Functor representing the 'known' predicate used in Prolog
     known = Functor("known", 2)
+    # Load 'askables' data from a JSON file
     with open("askables.json") as f:
         askables = json.load(f)
+    # Load 'cafes' data from a JSON file containing the names and Google Maps links
     with open("cafes.json") as f2:
         cafes = json.load(f2)
-
+    
+    # Function that interfaces with Prolog to handle user inputs
     def read_input(A, V, Y):
+         # Check if the third argument is a variable
         if isinstance(Y, Variable):
             askable = str(A)
+            # Retrieve the question data from the 'askables'
             ask_dict = askables[askable]
+            
             shared_data["curr_question"] = {
                 "id": askable,
                 "text": ask_dict["text"],
@@ -27,28 +34,37 @@ def run_expert_system(shared_data):
             # wait for response
             while f"{askable}_response" not in shared_data:
                 pass
+            # Retrieve the response
             response = shared_data[f"{askable}_response"]  # a list
             if askable in ["diet_restrictions", "arrive_times"]:
+                 # For list-based responses, we need to convert each item to a Prolog atom and unify
                 Y.unify(list(map(Atom, response)))
             elif askable in ["travel_distance", "length_stay"]:
+                # For numeric responses, we need to find the corresponding numeric value and unify
                 value_opt = next(
                     opt for opt in ask_dict["options"] if opt["id"] == response[0]
                 )
                 Y.unify(float(value_opt["value"]))
             else:
+                # For single-choice responses
                 Y.unify(Atom(response[0]))
             return True
         else:
             return False
-
+    # Set the arity of the read_input function, which is the number of arguments it accepts
     read_input.arity = 3
 
     registerForeign(read_input)
-
-    prolog.consult("expert.pl")  # open the KB for consulting
+    
+    # Load the Prolog knowledge base
+    prolog.consult("expert.pl")
+    
+    # Reset the 'known' predicate in the KB to delete any prior user responses
     call(retractall(known))
+    
     rec: list = [s for s in prolog.query("recommend(X)", maxresult=1)]
     shared_data["done"] = True
+    # Extract the recommendation from the query result
     rec_id = rec[0]["X"] if rec else None
     shared_data["rec"] = cafes[rec_id] if rec_id else None
     shared_data["rec_id"] = rec_id
